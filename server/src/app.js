68224771416d10a7c7d239b1b62420f3dcbf1c3a
Indomet <mehmetasim35@hotmail.com>
2023-11-08 14:55:18 +0100

    add .env file

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 //variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MapServiceDB';
 const port = process.env.PORT || 8081;
+
 // Connect to MongoDB
 mongoose.connect(mongoURI).catch(function (err) {
     if (err) {
@@ -21,6 +25,19 @@ const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+
+const env = app.get('env');
+// eslint-disable-next-line no-unused-vars
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: env === 'development' ? err : {}
+    });
+});
+
+// app.use('/api', controller);
 
 
 app.listen(port, function (err) {
