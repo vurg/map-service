013f8849a9d52490bcf3474effe72215a7ccffad
Indomet <mehmetasim35@hotmail.com>
2023-11-11 20:26:23 +0100

    #3 implement mqtt

--- server/src/app.js
+++ server/src/app.js
@@ -5,14 +5,15 @@ const morgan = require('morgan')
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const clinicsRouter = require('./routes/clinics');
+const mqttHandler = require('./mqtt');
 
 // load env variables
-dotenv.config({ path:'../.env'})
+dotenv.config({ path: '../.env' })
 
 //variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MapServiceDB';
 const port = process.env.PORT || 8081;
- 
+
 // Connect to MongoDB
 mongoose.connect(mongoURI).catch(function (err) {
     if (err) {
@@ -28,6 +29,18 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use(cors())
 
+const mqttClient = new mqttHandler();
+mqttClient.connect();
+
+app.post("/send-mqtt", function (req, res) {
+    mqttClient.sendMessage(req.body.message);
+    res.status(200).send("Message sent via mqtt");
+});
+
+
+//routes
+app.use('/api/v1', clinicsRouter);
+
 const env = app.get('env');
 // eslint-disable-next-line no-unused-vars
 app.use(function (err, req, res, next) {
@@ -37,10 +50,6 @@ app.use(function (err, req, res, next) {
         error: env === 'development' ? err : {}
     });
 });
-
-//routes
-app.use('/api/v1', clinicsRouter);
-
 
 app.listen(port, function (err) {
     if (err) throw err;
