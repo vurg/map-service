9db5117bee974fe96ffdf943e827f80d985a1d8a
Indomet <mehmetasim35@hotmail.com>
2023-11-12 13:02:10 +0100

    #4 disable the API functionality when running in a CI environment

--- server/src/models/ClinicModel.js
+++ server/src/models/ClinicModel.js
@@ -31,15 +31,17 @@ const ClinicSchema = new mongoose.Schema({
     }
 })
 
-ClinicSchema.pre('save', async function (next) {
-    const locationData = await geocoder.geocode(this.adress);
-    this.location = {
-        type: 'Point',
-        coordinates: [locationData[0].longitude, locationData[0].latitude], //this is a array of coordinates
-        formattedAddress: locationData[0].formattedAddress
-    }
-    this.adress = undefined;    // this will prevent his from getting saved to the database and instead we will save it as formattedAddress
-    console.log(this.location);
-    next();
-})
+if (process.env.CI !== 'true') {
+    ClinicSchema.pre('save', async function (next) {
+        const locationData = await geocoder.geocode(this.adress);
+        this.location = {
+            type: 'Point',
+            coordinates: [locationData[0].longitude, locationData[0].latitude], //this is a array of coordinates
+            formattedAddress: locationData[0].formattedAddress
+        }
+        this.adress = undefined;    // this will prevent his from getting saved to the database and instead we will save it as formattedAddress
+        console.log(this.location);
+        next();
+    })
+}
 module.exports = mongoose.model('Clinic', ClinicSchema);
